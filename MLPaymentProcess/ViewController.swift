ba90606608bf0cafa42ae1716657a2430b72dc98
JomiMoldes <josemiguel.moldes@gmail.com>
2017-11-05 17:17:08 -0300

    project initalization

---
+++ MLPaymentProcess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLPaymentProcess
+//
+//  Created by MIGUEL MOLDES on 5/11/17.
+//  Copyright © 2017 MIGUEL MOLDES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
